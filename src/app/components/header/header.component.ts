b7294d4617876c1fe669b0d4275f87b19d36f46f
PRONOY <pronoybanik82@gmail.com>
2024-04-23 19:53:04 +0600

    first commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: `
+  <h1 class="bg-green-400">header section</h1>
+  `,
+  styles: ``
+})
+export class HeaderComponent {
+
+}
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+
+export class HomeComponent {
+  DataOfArray = [1, 2, 3, 4]
+}
